27442f6be3b21f0e2732967aba6ce5115db3006d
Jérémy Derdaele <jeremy.derdaele@gmail.com>
2020-09-25 10:12:10 +0200

    Add time difference question.

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,12 @@ struct Time {
     minutes: Minutes,
 }
 
+#[derive(PartialEq, Eq)]
+struct Duration {
+    hours: Hours,
+    minutes: Minutes,
+}
+
 impl Add<&Minutes> for &Time {
     type Output = Time;
 
@@ -55,6 +61,15 @@ impl Add<&Minutes> for &Time {
             hours: Hours((self.hours.0 + (self.minutes.0 + minutes.0) / 60) % 24),
             minutes: Minutes((self.minutes.0 + minutes.0) % 60),
         }
+    }
+}
+
+fn difference(time_a: &Time, time_b: &Time) -> Duration {
+    let hours_diff = i8::abs((time_a.hours.0 as i8) - (time_b.hours.0 as i8)) as u8;
+    let minutes_diff = i8::abs((time_a.minutes.0 as i8) - (time_b.minutes.0 as i8)) as u8;
+    Duration {
+        hours: Hours(hours_diff),
+        minutes: Minutes(minutes_diff),
     }
 }
 
@@ -74,19 +89,27 @@ impl Distribution<Time> for Standard {
 }
 
 enum Question {
-    Time {
+    TimePlusMinutes {
         base_time: Time,
         minutes_diff: Minutes,
+    },
+    TimeDifference {
+        start_time: Time,
+        end_time: Time,
     },
 }
 
 impl fmt::Display for Question {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Question::Time {
+            Question::TimePlusMinutes {
                 base_time,
                 minutes_diff,
             } => write!(f, "{} + {}m ?", base_time, minutes_diff.0),
+            Question::TimeDifference {
+                start_time,
+                end_time,
+            } => write!(f, "Difference between {} and {} ?", end_time, start_time),
         }
     }
 }
@@ -104,28 +127,52 @@ impl FromStr for Time {
     }
 }
 
+impl FromStr for Duration {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, ()> {
+        let coords: Vec<&str> = s.trim().split('h').collect();
+
+        let hours = Hours(coords[0].parse().unwrap());
+        let minutes = Minutes(coords[1].parse().unwrap());
+
+        Ok(Duration { hours, minutes })
+    }
+}
+
 impl Distribution<Question> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Question {
-        let question_pool = [("Time", 1)];
+        let question_pool = [("TimePlusMinutes", 2), ("TimeDifference", 1)];
 
         match question_pool.choose_weighted(rng, |q| q.1).unwrap().0 {
-            "Time" => Question::Time {
+            "TimePlusMinutes" => Question::TimePlusMinutes {
                 base_time: rng.gen(),
-                minutes_diff: Minutes(rng.gen_range(1, 61)),
+                minutes_diff: Minutes(rng.gen_range(1, 60)),
             },
-            _ => panic!("Unhandled question!")
+            "TimeDifference" => Question::TimeDifference {
+                start_time: rng.gen(),
+                end_time: rng.gen(),
+            },
+            _ => panic!("Unhandled question!"),
         }
     }
 }
 
 fn is_correct(input: &str, question: &Question) -> bool {
     match question {
-        Question::Time {
+        Question::TimePlusMinutes {
             base_time,
             minutes_diff,
         } => {
             let time: Time = input.parse().unwrap();
             time == base_time + minutes_diff
+        }
+        Question::TimeDifference {
+            start_time,
+            end_time,
+        } => {
+            let duration: Duration = input.parse().unwrap();
+            duration == difference(start_time, end_time)
         }
     }
 }
